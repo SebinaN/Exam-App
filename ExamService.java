e764b8b473a4503974976db02c776a4b064bfb26
DA M1 55 <graciousmtileni@gmail.com>
2024-06-12 14:55:44 +0200

    question array completed

--- ExamService.java
+++ ExamService.java
@@ -11,3 +11,10 @@ public class ExamService implements Quiz {
 
         String[] answers_2 = { "The throw keyword", "The function that caused the error", "The catch block", "An object of the exception class", };
         questions[2] = new Exam("Additional information sent when an exception is thrown may be placed in : ", answers_2, "The catch block");
+
+        String[] answers_3 = { "Java keywords cannot be used as variable names", "All of the options", "Variable names are case-sensitive", "The first character must be a letter" };
+        questions[3] = new Exam("Which of the following are rules to declare variables in Java?", answers_3, "all of the options");
+
+        String[] answers_4 = { "Execute a java program", "Debug a java program", "interpret a java program", "Compile a java program" };
+        questions[4] = new Exam("What is the use of 'javac' command?", answers_4, "Compile a java program");
+    }
